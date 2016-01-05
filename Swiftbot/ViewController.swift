737b977458734770a58d2655de81b0f14bd68847
Reda Lemeden <git@kaishin.haz.email>
2016-01-05 14:38:13 +0100

    Initial commit

---
+++ Swiftbot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftbot
+//
+//  Created by Reda Lemeden on 05/01/2016.
+//  Copyright © 2016 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
